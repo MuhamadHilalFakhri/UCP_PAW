ae45b78cf4e4e7874c7e2ee47e61f71a10de8c54
hilal555 <muhamadhilal04@gmail.com>
2024-12-27 20:36:59 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+const todoRoutes = require('./routes/produkdb.js');
+require('dotenv').config();
+const port = process.env.PORT;
+const db = require('./database/db');
+const expressLayouts = require('express-ejs-layouts')
+const session = require('express-session');
+const authRoutes = require('./routes/authRoutes');
+const { isAuthenticated } = require('./middlewares/middleware.js');
+
+app.use('/public', express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(expressLayouts);
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+// Konfigurasi express-session
+app.use(session({
+    secret: process.env.SESSION_SECRET, 
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false }
+}));
+app.use('/', authRoutes);
+
+
+//app.use('/todos', todoRoutes);
+app.set('view engine', 'ejs');
+
+
+
+app.get('/',isAuthenticated, (req, res) => {
+    res.render('index',{
+        layout: 'layouts/main-layout'
+    });
+});
+app.get('/contact', isAuthenticated, (req, res) => {
+    res.render('contact',{
+        layout: 'layouts/main-layout'
+    });
+});
+
+app.get('/produk-view', isAuthenticated,(req, res) => {
+    db.query('SELECT * FROM produk', (err, todos) => {
+        if (err) return res.status(500).send('Internal Server Error');
+        res.render('produk', {
+            layout: 'layouts/main-layout',
+            todos: todos
+        });
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
